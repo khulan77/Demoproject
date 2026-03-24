78e64a7ea6145ac112215113094e0ad4647f603f
m2 <dhulan174@gmail.com>
2026-03-24 09:22:41 +0800

    Initial commit

---
+++ client/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import Three from "./about/components/Three";
+import Hero from "./components/Hero";
+export default function Home() {
+  return (
+    <div>
+      <main>
+           <Hero />
+      <Three />
+      </main>
+    </div>
+  );
+}
